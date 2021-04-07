2e2a5164e575ba5dea03d29c7080aec45979966d
pablojr17 <pablojr17@gmail.com>
2021-04-06 22:35:11 -0300

    Inserindo transações e seeds

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -20,9 +21,10 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
+    const data = {
       title, value, category, type
-    })
+    }
+    api.post('/transaction', data)
   }
 
   return (
